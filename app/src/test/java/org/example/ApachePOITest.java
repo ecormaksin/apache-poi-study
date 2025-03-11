2ea5d7be7e87591ac6a3f2fd8ce19234fa754f9b
Makoto Oda <truth_jp_4133@yahoo.co.jp>
2025-03-11 09:23:08 +0900

    test with an empty doc file

--- app/src/test/java/org/example/ApachePOITest.java
+++ app/src/test/java/org/example/ApachePOITest.java
@@ -1,13 +1,14 @@
 package org.example;
 
 import org.apache.poi.hwpf.HWPFDocument;
-import org.apache.poi.hwpf.usermodel.Range;
-import org.apache.poi.xwpf.usermodel.BodyType;
 import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import java.io.*;
+import java.io.File;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.nio.file.Files;
 
 public class ApachePOITest {
@@ -15,11 +16,20 @@ public class ApachePOITest {
     @ParameterizedTest
     @ValueSource(strings = {"template_without_table.doc", "template_with_table.doc"})
     void testHWPF(String templateFileName) throws Exception {
+        testHWPFPerFile(templateFileName);
+    }
+
+    @Test
+    void testHWPFStepByStep() throws Exception {
+        testHWPFPerFile("template_with_table_step_by_step.doc");
+    }
+
+    private void testHWPFPerFile(String templateFileName) throws Exception {
         File templateFile = new File("src/main/resources/" + templateFileName);
         File outputFile = new File("/tmp/" + templateFileName);
 
         try (InputStream inputStream = Files.newInputStream(templateFile.toPath());
-             HWPFDocument aHWPFDocument = new HWPFDocument(inputStream);) {
+             HWPFDocument aHWPFDocument = new HWPFDocument(inputStream)) {
 
             try (OutputStream outputStream = Files.newOutputStream(outputFile.toPath())) {
                 aHWPFDocument.write(outputStream);
@@ -27,8 +37,8 @@ public class ApachePOITest {
         }
 
         try( InputStream inputStream = Files.newInputStream(outputFile.toPath());
-        HWPFDocument aHWPFDocument = new HWPFDocument(inputStream);) {
-            Range aRange = aHWPFDocument.getRange();
+             HWPFDocument aHWPFDocument = new HWPFDocument(inputStream)) {
+            aHWPFDocument.getRange();
         }
     }
 
@@ -39,7 +49,7 @@ public class ApachePOITest {
         File outputFile = new File("/tmp/" + templateFileName);
 
         try (InputStream inputStream = Files.newInputStream(templateFile.toPath());
-             XWPFDocument aXWPFDocument = new XWPFDocument(inputStream);) {
+             XWPFDocument aXWPFDocument = new XWPFDocument(inputStream)) {
 
             try (OutputStream outputStream = Files.newOutputStream(outputFile.toPath())) {
                 aXWPFDocument.write(outputStream);
@@ -47,8 +57,8 @@ public class ApachePOITest {
         }
 
         try( InputStream inputStream = Files.newInputStream(outputFile.toPath());
-             XWPFDocument aHWPFDocument = new XWPFDocument(inputStream);) {
-            BodyType aBodyType = aHWPFDocument.getPartType();
+             XWPFDocument aHWPFDocument = new XWPFDocument(inputStream)) {
+            aHWPFDocument.getPartType();
         }
     }
 }
